ae3dfabfd74fd1ae09367c7c5b73ee742673e088
Hoover Becerra <hoover.hernandez@escueladidactica.com>
2023-12-14 12:40:37 -0500

    Add datatable

--- static/js/table.js
+++ static/js/table.js
@@ -3,8 +3,9 @@ document.addEventListener("DOMContentLoaded", init);
 function init() {
     const dataTable = new DataTable('#example', {
         lengthMenu: [5, 10, 30, 100],
+        dom: 'Blfrtip',
         buttons: [
-            'copy', 'excel', 'pdf'
+            'excel', 'pdf'
         ]
     });
 }
